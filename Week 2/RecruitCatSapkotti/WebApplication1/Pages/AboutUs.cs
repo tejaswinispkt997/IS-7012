c761fcf38ae9854357afeff5e9657cc8e2154c79
Mamta Gupta <mamta.gupta2@dhl.com>
2022-09-07 13:19:13 -0400

    recruitment App

---
+++ Week 2/RecruitCatSapkotti/WebApplication1/Pages/AboutUs.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Pages
+{
+    public class AboutUs : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
